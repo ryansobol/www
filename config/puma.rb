a0cee0f54fde6e0cd04adee1bdf20b0ac2f75469
Ryan Sobol <contact@ryansobol.com>
2013-11-30 09:12:09 +0800

    Use PUMA_THREADS instead

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,8 @@ port ENV["PORT"]
 
 rackup "mango.ru"
 
-threads ENV["PUMA_MIN_THREADS"] || 0,
-        ENV["PUMA_MAX_THREADS"] || 5
+threads ENV["PUMA_THREADS"] || 5,
+        ENV["PUMA_THREADS"] || 5
 
 workers ENV["PUMA_WORKERS"] || 4
 
