f1043379e312d450c4c10ae81d6268342bbd2a65
Ryan Sobol <contact@ryansobol.com>
2013-11-30 10:56:44 +0800

    Adjustments to puma and new relic

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,6 @@
-port ENV["PORT"]
-
-rackup "mango.ru"
-
-threads ENV["PUMA_THREADS"] || 5,
-        ENV["PUMA_THREADS"] || 5
-
+rackup  "mango.ru"
+port    ENV["PORT"]
+threads ENV["PUMA_THREADS"] || 12, ENV["PUMA_THREADS"] || 12
 workers ENV["PUMA_WORKERS"] || 4
 
 preload_app!
