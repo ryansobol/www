89b2c1c3ef79e08133b828fab22dcc4fe273da15
Ryan Sobol <contact@ryansobol.com>
2013-11-30 09:10:32 +0800

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,8 @@ port ENV["PORT"]
 rackup "mango.ru"
 
 threads ENV["PUMA_MIN_THREADS"] || 0,
-        ENV["PUMA_MIN_THREADS"] || 5
+        ENV["PUMA_MAX_THREADS"] || 5
 
-workers ENV["PUMA_WORKERS"] || 3
+workers ENV["PUMA_WORKERS"] || 4
 
 preload_app!
