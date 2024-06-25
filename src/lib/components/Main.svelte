932a76bdc4d001590cbcfdb9bc0a13ede52c9fa4
Ryan Sobol <hello@ryansobol.com>
2024-06-25 07:18:53 -0700

    Simplify cta href decoding algorithm

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -5,6 +5,7 @@
 
 	onMount(() => {
 		const encoded =
+			'&#109;&#97;&#105;&#108;&#116;&#111;:' +
 			'&#104;&#101;&#108;&#108;&#111;@' +
 			'&#114;&#121;&#97;&#110;&#115;&#111;&#98;' +
 			'&#111;&#108;&#46;&#99;&#111;&#109;&#63;' +
@@ -12,9 +13,7 @@
 			'&#61;&#76;&#101;&#116;&#37;&#50;&#55;&#115;' +
 			'&#37;&#50;&#48;&#99;&#104;&#97;&#116;';
 
-		const decoded = encoded.replace(/&#(\d+);/g, (_, code) => String.fromCharCode(code));
-
-		ctaBinding.href = 'mai' + 'lto' + ':' + decoded;
+		ctaBinding.href = encoded.replace(/&#(\d+);/g, (_, code) => String.fromCharCode(code));
 	});
 </script>
 
