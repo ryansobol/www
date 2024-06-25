0d272cf27efd7d5d2cb0a5be2c72f7a8b23ef91f
Ryan Sobol <hello@ryansobol.com>
2024-06-24 23:35:21 -0700

    Refactor into a Footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,40 @@
+<footer id="license">
+	<section>
+		<h6>
+			This website by <a
+				href="http://ryansobol.com"
+				property="cc:attributionName"
+				rel="cc:attributionURL">Ryan Sobol</a
+			>
+			is licensed under a
+			<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"
+				>Creative Commons Attribution-ShareAlike 4.0 International License</a
+			>.
+		</h6>
+	</section>
+</footer>
+
+<style lang="scss">
+	#license {
+		opacity: 0;
+
+		-webkit-animation: fadeIn 0.2s ease-out 0.4s 1 normal forwards;
+		animation: fadeIn 0.2s ease-out 0.4s 1 normal forwards;
+
+		h6 {
+			margin: 0;
+			padding: 40px 0 20px 0;
+			font-size: 14px;
+			text-align: center;
+			font-weight: 300;
+		}
+
+		a {
+			color: $midnightBlue;
+		}
+
+		a:hover {
+			color: $belizeHole;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import BottomNav from '$lib/components/BottomNav.svelte';
+	// import Footer from '$lib/components/Footer.svelte';
 	import Hero from '$lib/components/Hero.svelte';
 	import Main from '$lib/components/Main.svelte';
 	import Progress from '$lib/components/Progress.svelte';
@@ -18,10 +19,4 @@
 
 <Main />
 
-<!-- <footer id="license">
-  <section>
-    <h6>
-        This website by <a xmlns:cc="http://creativecommons.org/ns#" href="http://ryansobol.com" property="cc:attributionName" rel="cc:attributionURL">Ryan Sobol</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
-    </h6>
-  </section>
-</footer> -->
+<!-- <Footer /> -->
